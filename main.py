4ea349ec65a50bd9b8b7e281c76a4e7b246fb4a2
sarahmarina88 <sarahmleenhouts@gmail.com>
2021-03-24 12:31:11 +0000

    changes in main after creating skeleton of classes

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from BlackJackPcg.Deck import Deck
 
 
-class Player:
-    pass
-
-
 if __name__ == '__main__':
     d = Deck()
     d.init_deck()
     d.print_deck()
     print()
-    d.suffle_deck()
+    d.shuffle_deck()
     d.print_deck()
-    print()
+
