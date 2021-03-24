dc5375dced781b28ed3a340a8808aeb01625960a
sarahmarina88 <sarahmleenhouts@gmail.com>
2021-03-24 12:28:06 +0000

    Outline of Deck class

--- BlackJackPcg/Deck/__init__.py
+++ BlackJackPcg/Deck/__init__.py
@@ -10,17 +10,26 @@ class Deck:
         self.deck = []
 
     def init_deck(self):
-        for i_col in CardUtils.get_possible_colors():
-            for i_suit in CardUtils.get_possible_suits():
-                for i_num in CardUtils.get_possible_numbers():
-                    self.deck.append(Card(i_suit, i_col, i_num))
+        #go through the possible suits (4) and for each get the possible numbers then mark as red or blue for colour depending on the suit - colour not actually needed
+        for i_suit in CardUtils.get_possible_suits():
+            for i_num in CardUtils.get_possible_numbers():
+                if i_suit == 'S' or i_suit == 'C':
+                    i_col = 'B'
+                else:
+                    i_col = 'R'
+                #each of the cards added to list of cards in deck
+                self.deck.append(Card(i_suit, i_col, i_num))
 
     def get_deck(self):
         return self.deck
 
-    def suffle_deck(self):
+    def shuffle_deck(self):
         random.shuffle(self.deck)
 
     def print_deck(self):
         for card in self.deck:
             card.show()
+
+    def give_cards(self, num):
+        pass
+    #takes the num card/s off deck, return list of cards dealt
