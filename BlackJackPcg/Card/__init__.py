531bbaf9541a876a7909306b1c897868cca0c0ca
sarahmarina88 <sarahmleenhouts@gmail.com>
2021-03-31 20:58:17 +0100

    removed colour from card

--- BlackJackPcg/Card/__init__.py
+++ BlackJackPcg/Card/__init__.py
@@ -3,22 +3,15 @@ from BlackJackPcg.Utils import CardUtils
 
 class Card:
 
-    def __init__(self, suit, color, number):
+    def __init__(self, suit, number):
         if number not in CardUtils.get_possible_numbers():
             raise RuntimeError("the number passed to the card is not valid")
 
         if suit not in CardUtils.get_possible_suits():
             raise RuntimeError("the suit passed to the card is not valid")
 
-        if color not in CardUtils.get_possible_colors():
-            raise RuntimeError("the number passed to the card is not valid")
-
         self.suit = suit
-        self.color = color
         self.number = number
-
-    def get_color(self):
-        return self.color
 
     def get_number(self):
         return self.number
@@ -30,4 +23,4 @@ class Card:
         print(self.to_string())
 
     def to_string(self):
-        return "The card is: {}{}{}".format(self.number, self.suit, self.color)
+        return "The card is: {}{}".format(self.number, self.suit)
