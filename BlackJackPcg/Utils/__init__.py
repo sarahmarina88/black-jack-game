4b3dd60fdb3087303ccfd6afc11e09dd15aea155
sarahmarina88 <sarahmleenhouts@gmail.com>
2021-03-31 20:59:22 +0100

    removed colour from utils

--- BlackJackPcg/Utils/__init__.py
+++ BlackJackPcg/Utils/__init__.py
@@ -5,10 +5,6 @@ class CardUtils:
         return ["S", "C", "H", "D"]
 
     @staticmethod
-    def get_possible_colors():
-        return ["R", "B"]
-
-    @staticmethod
     def get_possible_numbers():
         possible_numbers = ["{}".format(x) for x in range(2, 11)]
         possible_numbers.extend(["J", "Q", "K", "A"])
