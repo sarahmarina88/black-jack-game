a24794b24264e299584ef7a5d4d73f3d75a176ae
sarahmarina88 <sarahmleenhouts@gmail.com>
2021-06-09 23:01:29 +0100

    editing tests of function with input from user

--- BlackJackPcg/Person/Player/test/test_player.py
+++ BlackJackPcg/Person/Player/test/test_player.py
@@ -1,3 +1,5 @@
+import contextlib
+import io
 from unittest import TestCase, mock
 
 from BlackJackPcg.Person.Player import Player
@@ -21,40 +23,37 @@ class TestPlayer(TestCase):
     def test_make_game_decision_gives_error_for_invalid_integer_input(self):
         p = Player("test_player")
         #make the input so it will be three
-        with mock.patch("builtins.input", return_value=3):
-            #this should return a print statement - also mock the print but also cause an exception otherwise loop won't ever break
-            #side effect - to happen when the mock is called so as to break the loop
-            with mock.patch("builtins.print", side_effect=[Exception("To break the while loop")]) as mocked_print:
-                #exception will be reaised as a statement is printed
-                with self.assertRaises(Exception) as cm:
+        with mock.patch("builtins.input", side_effect=[3,1]):
+            #this should return a print statement - redirect to buffer
+            # #side effect - two different inputs, second will break loop
+            with io.StringIO() as buffer:
+                with contextlib.redirect_stdout(buffer):
                     p.make_game_decision()
-                    the_exception_raised = cm.exception
-                #check that the print was called and with the expected message; side effect was an exception which was raised - can check exception raised
-                mocked_print.assert_called_with("Please enter only 1 for yes or 0 for no.") and self.assertTrue(the_exception_raised == "To break the while loop")
+                    printed_text = buffer.getvalue()
+                    self.assertTrue(printed_text.count("Please enter only 1 for yes or 0 for no.") ==1)
 
-    def test_make_game_decision_gives_value_error_when_string_as_input(self):
-        pass
-        # mock to input a string - need to check that a ValueError results
-
-    def test_make_game_decision_prints_message_when_string_as_input(self):
+    def test_make_game_decision_gives_expected_message_when_string_as_input(self):
+        # don't have to test for both error and message - the error is only way to get the message
+        # by giving list of inputs can break the loop - also checking can keep inputting till correct
         p = Player("test_player")
-        # mock to input a string - should cause a value error and then print message - also mock the print to check is correct
-        with mock.patch("builtins.input", return_value = 'one'):
-            # mock for the print after value error from str input
-            # will continue as a loop - make a side effect of mocking the print is to give an Exception - breaks the while loop
-            with mock.patch("builtins.print", side_effect=[Exception("To break the while loop")]) as mocked_print:
-                with self.assertRaises(Exception):
+        # mock to input a string - need to check that a ValueError results
+        with mock.patch("builtins.input", side_effect=['one', 'two', 1]):
+            with io.StringIO() as buffer:
+                # redirect the stdout to the buffer - whatever goes to stdout will go to buffer
+                with contextlib.redirect_stdout(buffer):
                     p.make_game_decision()
-            # assert whether the expected print statement was shown
-            mocked_print.assert_called_with("Please enter decision to take a card as an integer (0 for no, 1 for yes)")
+                    printed_text = buffer.getvalue()
+                    #check that the printed text contains the intended message two times - as final value input is 1 will return true
+                    self.assertTrue(printed_text.count("Please enter decision to take a card as an integer (0 for no, 1 for yes)") == 2)
 
     def test_function_gives_message_and_keeps_asking_input_until_valid(self):
         p = Player("test_player")
         # mock to input different inputs in sequence - first three should cause print message, last (1) correct so breaks - also mock the print to check is correct
         with mock.patch("builtins.input", side_effect=['one', 3, 3.5, 1]):
-            # mock for the printed messages after value error from string input or invalid numbers
-            # will continue as a loop as without exception side effect of mocked print - last input of list correct to break loop
-            with mock.patch("builtins.print") as mocked_print:
-                p.make_game_decision()
-            # gave three incorrect inputs before valid input so should have three times printed something
-            self.assertTrue(mocked_print.call_count == 3)
+            with io.StringIO() as buffer:
+                with contextlib.redirect_stdout(buffer):
+                    p.make_game_decision()
+                    printed_text = buffer.getvalue()
+                    self.assertTrue(printed_text.count("Please enter") == 3)
+
+
